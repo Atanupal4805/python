b22d19a643d7ed236911f37182affe4fb84c8c7f
Atanupal <suzumenaruto11@gmail.com>
2024-10-04 03:19:23 +0530

    added basics of python file

---
+++ basics.py
@@ -0,0 +1,69 @@
+# "write  a Python program to print "Hello" world" 
+print("hello world!")
+print("hi i am Atanu")
+
+
+# Write a Python program to print your name and age.
+print("my name is atanu.My age is 19.")
+
+
+# Write a Python program to take input from the user and display it.
+num = input("enter a number")
+print("you have entered:",num)
+
+
+# Write a Python program to add two numbers entered by the user.
+# Write a Python program to find the product of two numbers.
+a=18
+b=45
+print('sum:',a+b)
+print('sub:',a-b)
+print('multi:',a*b)
+print('div:',a/b)
+print('mod:',b%a)
+
+
+# Write a Python program to find the square of a number.
+num=float(input("enter a number:"))
+print ('the entered number is :',num)
+print("the square of the number is :",a**b)
+square=num*num
+print(f"The square of {num} is {square}.")
+
+
+# Write a Python program to calculate the area of a circle.
+radius=float(input('enter the circle radius:'))
+area=3.14*(radius*radius)
+print(f'you have entered the radius {radius} so the area is {area}:')
+
+
+# Write a Python program to calculate the perimeter of a rectangle.
+length=float(input('enter the length '))
+width=float(input('enter the width '))
+perimeter=2*(length+width)
+print(f'the length and width of the rectangle is {length} {width} so the perimeter of the rectangle is {perimeter}')
+
+
+# Write a Python program to swap two numbers.
+a=10
+b=20
+temp=a
+a=b
+b=temp
+print(f'the value of a is :{a} and the value of b is : {b}')
+
+
+# Write a Python program to print the type of variables.
+int_variable= int(input('enter a number:'))
+float_variable= float(input('enter a number:'))
+string_variable= (input('enter a string:'))
+print(f'the type of the variable {int_variable} is : {type(int_variable)} ')
+print(f'the type of the variable {float_variable} is : {type(float_variable)} ')
+print(f'the type of the string {string_variable} is : {type(string_variable)} ')
+
+
+
+
+
+
+
